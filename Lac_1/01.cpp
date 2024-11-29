9f71c1a45209c78779788a8d6146d2673a288e50
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-11-29 22:07:30 +0530

    upgrade code

--- Lac_1/01.cpp
+++ Lac_1/01.cpp
@@ -1,8 +1,13 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cout<<"Hey Yash!"<<"\n";
-    cout<<"Hey Yash!";
+    // cout<<"Hey Yash!"<<"\n";
+    // cout<<"Hey Yash!";
+
+    int x, y;
+    cout<<"Enter the X and Y value: ";
+    cin>>x>>y;
+    cout<<"value of x is: "<<x << " and y is: "<<y;
     return 0;
 }
